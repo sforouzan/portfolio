384208f66a58bf7be35789ad5cd715cafc7194ec
tyrellcurry <tyrellcurryio@gmail.com>
2026-05-04 21:14:54 -0700

    chore: eslint and ts

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -9,6 +9,11 @@ const compat = new FlatCompat({
   baseDirectory: __dirname,
 });
 
-const eslintConfig = [...compat.extends("next/core-web-vitals")];
+const eslintConfig = [
+  ...compat.extends("next/core-web-vitals").map((config) => ({
+    ...config,
+    files: config.files ?? ["**/*.{js,mjs,cjs,jsx,ts,tsx}"],
+  })),
+];
 
 export default eslintConfig;
